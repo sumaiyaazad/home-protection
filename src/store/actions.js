fa2eb7376c6c2e35496932ec06b4ac8f62d58bb1
sumaiyaazad <azadsumaiya00@gmail.com>
2021-07-17 21:34:49 +0600

    version 1 of home protection

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,76 @@
-export default {
+import router from "@/router";
+import axios from 'axios';
+let send;
+let response;
 
+const fetchFlame=async()=>{
+        try {
+            response = await axios.get(' https://api.thingspeak.com/channels/1446074/fields/3.json');
+            send=modifiedData(response.data.feeds);
+            console.log("fetchFlame response: ",send);
+            return send;
+        } catch (e) {
+            console.log("fetchFlame catch error response data: ",e.message);
+        }
+    };
+const fetchGasLpg=async()=>{
+        try {
+            response = await axios.get('https://api.thingspeak.com/channels/1446074/fields/1.json');
+            send=modifiedData(response.data.feeds);
+            console.log("fetchGasLpg response: ",send);
+            return send;
+        } catch (e) {
+            console.log("fetchGasLpg catch error response data: ",e.message);
+        }
+    };
+const fetchGasSmoke= async ()=>{
+        try {
+            response = await axios.get('https://api.thingspeak.com/channels/1446074/fields/2.json');
+            send=modifiedData(response.data.feeds);
+            console.log("fetchGasSmoke response: ",send);
+            return send;
+        } catch (e) {
+            console.log("fetchGasSmoke catch error response data: ",e.message);
+        }
+    };
+const fetchAT= async ()=>{
+    try {
+        response = await axios.get('https://api.thingspeak.com/channels/1446074/fields/5.json');
+        send=modifiedData(response.data.feeds);
+        console.log("fetchAT response: ",send);
+        return send;
+    } catch (e) {
+        console.log("fetchAT catch error response data: ",e.message);
+    }
+};
+const fetchMD= async ()=>{
+    try {
+        response = await axios.get('https://api.thingspeak.com/channels/1446074/fields/4.json');
+        send=modifiedData(response.data.feeds);
+        console.log("fetchMD response: ",send);
+        return send;
+    } catch (e) {
+        console.log("fetchMD catch error response data: ",e.message);
+    }
+};
+const postAT= async (value)=>{
+    try {
+        await axios.post('https://api.thingspeak.com/update?api_key=KRJ5C7MAMRWHSSKO&field5='+value);
+        return true;
+    } catch (e) {
+        console.log("fetchMD catch error response data: ",e.message);
+    }
+};
+
+const modifiedData= (array)=>{
+        let init= array.length>5 ? (array.length-5) : 0;
+        array=array.slice(init,array.length);
+        for(let i=0;i<array.length;i++)
+        {
+            array[i].created_at=(new Date(Date.parse(array[i].created_at))).toLocaleString();
+        }
+        return array;
+    };
+export default{
+fetchFlame, fetchGasLpg, fetchGasSmoke, fetchAT, fetchMD, postAT
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,43 @@
 <template>
-        <v-container fluid style="margin-top: 3%;">
+        <v-container fluid style="margin-top: 0%;">
             <v-row>
-                <v-col cols="12" lg="4" sm="2"></v-col>
-                <v-col cols="12" lg="4" sm="8">
+                <v-col cols="12" lg="3" sm="0"></v-col>
+                <v-col cols="12" lg="6" sm="12">
                     <v-card elevation="5" class="pa-16">
                         <v-card-title class=" justify-center" style="font-family: 'Dancing Script', cursive; font-size: 2em">Press Button</v-card-title>
+                        <v-card-actions class="justify-center">
+                            <div class="my-2">
+                                <v-btn
+                                    x-large
+                                    color="success"
+                                    style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.1em"
+                                    dark
+                                    @click="allClicked"
+                                >
+                                    All Sensor Status
+                                </v-btn>
+                                        <v-card-text v-if="allshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">temperature reading :
+                                        </v-card-text>
+                                        <v-card-text v-for="item in temp" :key="item.id" v-if="allshow && item.field3!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em"> {{item.created_at}}   :   {{item.field3}} C
+                                        </v-card-text>
+                                        <v-card-text v-if="allshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">lpg gas reading:
+                                        </v-card-text>
+                                        <v-card-text v-for="item in lpg" :key="item.id" v-if="allshow && item.field1!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field1}} ppm
+                                        </v-card-text>
+                                        <v-card-text v-if="allshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">smoke reading:
+                                        </v-card-text>
+                                        <v-card-text v-for="item in smoke" :key="item.id" v-if="allshow  && item.field2!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field2}} ppm
+                                        </v-card-text>
+                                        <v-card-text v-if="allshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">anti theft reading:
+                                        </v-card-text>
+                                        <v-card-text v-for="item in at" :key="item.id" v-if="allshow && item.field5!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field5}}
+                                        </v-card-text>
+                                        <v-card-text v-if="allshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">detection reading :
+                                        </v-card-text>
+                                        <v-card-text v-for="item in md" :key="item.id" v-if="allshow && item.field4!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field4}}
+                                        </v-card-text>
+                                    </div>
+                        </v-card-actions>
                         <v-card-actions class="justify-center">
                             <div class="my-2">
                             <v-btn
@@ -16,9 +49,9 @@
                             >
                                 Flame Sensor Status
                             </v-btn>
-                                <v-card-text v-if="fshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">current temperature :
+                                <v-card-text v-if="fshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">temperature reading :
                                 </v-card-text>
-                                <v-card-text v-if="fshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em"> 22 Celcius
+                                <v-card-text v-for="item in temp" :key="item.id" v-if="fshow && item.field3!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em"> {{item.created_at}}   :   {{item.field3}} C
                                 </v-card-text>
                             </div>
                         </v-card-actions>
@@ -33,13 +66,13 @@
                                 >
                                    Gas Sensor Status
                                 </v-btn>
-                                <v-card-text v-if="gshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">lpg gas amount:
+                                <v-card-text v-if="gshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">lpg gas reading:
                                 </v-card-text>
-                                <v-card-text v-if="gshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">1000 ppm
+                                <v-card-text v-for="item in lpg" :key="item.id" v-if="gshow && item.field1!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field1}} ppm
                                 </v-card-text>
-                                <v-card-text v-if="gshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">smoke amount:
+                                <v-card-text v-if="gshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">smoke reading:
                                 </v-card-text>
-                                <v-card-text v-if="gshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">1000 ppm
+                                <v-card-text v-for="item in smoke" :key="item.id" v-if="gshow && item.field2!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field2}} ppm
                                 </v-card-text>
                             </div>
                         </v-card-actions>
@@ -55,9 +88,26 @@
                                 >
                                     Anti-theft Sensor Status
                                 </v-btn>
-                                <v-card-text v-if="atshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">last motion detection time:
+                                <v-card-text v-if="atshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">anti theft reading:
                                 </v-card-text>
-                                <v-card-text v-if="atshow" style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.7em">1000 ms
+                                <v-card-text v-for="item in at" :key="item.id" v-if="atshow && item.field5!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field5}}
+                                </v-card-text>
+                            </div>
+                        </v-card-actions>
+                        <v-card-actions class="justify-center">
+                            <div class="my-2">
+                                <v-btn
+                                    x-large
+                                    color="success"
+                                    style="width:310px;font-family: 'Dancing Script', cursive; font-size: 1.1em"
+                                    dark
+                                    @click="mdClicked"
+                                >
+                                    Motion Detection Status
+                                </v-btn>
+                                <v-card-text v-if="mdshow" style="font-family: 'Dancing Script', cursive; font-size: 1.7em">detection reading :
+                                </v-card-text>
+                                <v-card-text v-for="item in md" :key="item.id" v-if="mdshow  && item.field4!==null" style="font-family: 'Dancing Script', cursive; font-size: 1.2em">{{item.created_at}}   :   {{item.field4}}
                                 </v-card-text>
                             </div>
                         </v-card-actions>
@@ -74,7 +124,7 @@
                         </v-card-actions>
                     </v-card>
                 </v-col>
-                <v-col cols="12" lg="4" sm="2">
+                <v-col cols="12" lg="3" sm="0">
                 </v-col>
             </v-row>
         </v-container>
@@ -82,6 +132,7 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
 
   export default {
     name: 'Home',
@@ -90,20 +141,78 @@
             atflag:false,
             atshow:false,
             gshow:false,
-            fshow:false
+            fshow:false,
+            mdshow:false,
+            allshow:false,
+            temp:[],
+            lpg:[],
+            smoke:[],
+            at:[],
+            md:[]
         }
     },
-    components: {
+    components: {},
+    watch: {
+        atflag: (newVal, oldVal)=> {
+            if(newVal===true){
+                this.postAT(1);
+            }else{
+                this.postAT(0);
+            }
+        }
     },
     methods:{
-        fClicked(){
-            this.fshow=!this.fshow;
+        ...mapActions(['fetchFlame','fetchGasLpg','fetchGasSmoke','fetchAT','fetchMD','postAT']),
+
+        async fClicked(){
+            if(this.fshow===false){
+                this.temp=await this.fetchFlame();
+                this.fshow=(this.temp.length===0 ? false : true);
+            }
+            else{
+                this.fshow=false;
+            }
         },
-        gClicked(){
-            this.gshow=!this.gshow;
+        async gClicked(){
+            if(this.gshow===false){
+                this.lpg=await this.fetchGasLpg();
+                this.smoke=await this.fetchGasSmoke();
+                this.gshow=(this.lpg.length===0 && this.smoke.length===0? false : true);
+            }
+            else{
+                this.gshow=false;
+            }
         },
-        atClicked(){
-            this.atshow=!this.atshow;
+        async atClicked(){
+            if(this.atshow===false){
+                this.at=await this.fetchAT();
+                this.atshow=(this.at.length===0 ? false : true);
+            }
+            else{
+                this.atshow=false;
+            }
+        },
+        async mdClicked(){
+            if(this.mdshow===false){
+                this.md=await this.fetchMD();
+                this.mdshow=(this.md.length===0 ? false : true);
+            }
+            else{
+                this.mdshow=false;
+            }
+        },
+        async allClicked(){
+            if(this.allshow===false){
+                this.temp=await this.fetchFlame();
+                this.lpg=await this.fetchGasLpg();
+                this.smoke=await this.fetchGasSmoke();
+                this.at=await this.fetchAT();
+                this.md=await this.fetchMD();
+                this.allshow=(this.temp.length===0 && this.lpg.length===0 && this.smoke.length===0 && this.at.length===0 && this.md.length===0 ? false : true);
+            }
+            else{
+                this.allshow=false;
+            }
         }
     }
   }
@@ -112,7 +221,7 @@
 <style>
 .my-switch .v-label {
     font-family: 'Dancing Script', cursive;
-    font-size: 1.5em;
+    font-size: 1.7em;
     text-decoration: black;
     color: black;
 }
